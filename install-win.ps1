2f38199127da6366bf6e8f81baded7da8d2a1a39
luma <tomorinao.info@gmail.com>
2019-12-29 20:32:12 +0900

    action

--- install-win.ps1
+++ install-win.ps1
@@ -3,5 +3,7 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.
 
 scoop install sudo
 
-sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
+sudo echo hello from sudo
+
+# sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
 
