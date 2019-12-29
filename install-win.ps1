51c498e8edef42ddcdcc0e1809dcf67b09afc88d
luma <tomorinao.info@gmail.com>
2019-12-29 20:56:01 +0900

    action

--- install-win.ps1
+++ install-win.ps1
@@ -1,11 +1,18 @@
 
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-iwr -useb get.scoop.sh | iex
+sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
 scoop install sudo
 
-echo hello from not sudo
-sudo echo hello from sudo
-
-# sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
+iwr -useb get.scoop.sh | iex
+if (-not(([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"`
+    ))) {
+    if ( $env:EXIT_NOT_ADMIN ) {
+      exit
+    }
+    sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
+} else {
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
+}
 
