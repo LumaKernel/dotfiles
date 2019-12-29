f946c9042f7a58f9a33e9bb0cf6c3ecad15de3a0
luma <tomorinao.info@gmail.com>
2019-12-29 20:39:38 +0900

    action

--- install-win.ps1
+++ install-win.ps1
@@ -1,8 +1,10 @@
 
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+iwr -useb get.scoop.sh | iex
 
 scoop install sudo
 
+echo hello from not sudo
 sudo echo hello from sudo
 
 # sudo Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/LumaKernel/dotfiles/master/install-win-after-sudo.ps1')
