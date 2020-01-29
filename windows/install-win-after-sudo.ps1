83956710417c317d8ceeb46e03b3670393f16b6e
Luma <tomorinao.info@gmail.com>
2020-01-30 06:50:25 +0900

    upd

--- windows/install-win-after-sudo.ps1
+++ windows/install-win-after-sudo.ps1
@@ -19,11 +19,20 @@ scoop install wsl-terminal
 scoop install python36
 # scoop install llvm
 
+echo "以下は自分で入れてください"
+echo "  - WSL"
+echo "  - wsltty"
+
 # -- git の設定
 git config --global core.ignorecase false
 git config --global core.quotepath false
 git config --global core.safecrlf true
 git config --global core.autocrlf false
+
+echo ""
+echo "以下を自分で実行してください"
+echo "git config --global user.name <name>"
+echo "git config --global user.email <email>"
 
 
 cd ~
@@ -39,9 +48,4 @@ concfg import ~/dotifles/windows/concfg/lumc-vs-code-dark-plus.json -n
 
 # -- install dein.vim
 git clone https://github.com/Shougo/dein.vim ~/.cache/dein/repos/github.com/Shougo/dein.vim
-
-
-echo "以下を自分で実行してください"
-echo "git config --global user.name <name>"
-echo "git config --global user.email <email>"
 
