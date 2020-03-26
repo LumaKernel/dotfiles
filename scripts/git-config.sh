31bd413c7de13e4b0480c1e84521937ddb85304b
Luma <tomorinao.info@gmail.com>
2020-03-27 00:18:43 +0900

    git のショートカット追加
    https://dzone.com/articles/lesser-known-git-commands

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -19,3 +19,20 @@ git config --global alias.lg3 !"git lg3-specific --all"
 git config --global alias.lg1-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
 git config --global alias.lg2-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
 git config --global alias.lg3-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'"
+
+# git push --force を弱めた版， ローカルのほうが新しい場合のみ
+git config --global alias.please "push --force-with-lease"
+git config --global alias.commend 'commit --amend --no-edit'
+git config --global alias.it '!git init && git commit -m “root” --allow-empty'
+
+$ git config --global alias.stsh 'stash --keep-index'
+$ git config --global alias.staash 'stash --include-untracked'
+$ git config --global alias.staaash 'stash --all'
+
+# git stsh      # stash only unstaged changes to tracked files
+# git stash     # stash any changes to tracked files
+# git staash    # stash untracked and tracked files
+# git staaash   # stash ignored, untracked, and tracked files
+
+git config --global alias.merc 'merge --no-ff'
+
