8fc31cb6290d9daef21fcc53503cbfc84c29cd7f
Luma <world@luma.email>
2021-08-05 01:21:42 +0900

    update gh

--- linux/bin/installers/install-gh.sh
+++ linux/bin/installers/install-gh.sh
@@ -5,11 +5,12 @@ SCRIPTS_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 # shellcheck source=../../utils/shared.sh
 source "$SCRIPTS_DIR/utils/shared.sh"
 
-VERSION="1.9.2"
+VERSION="1.14.0"
 
 cd /tmp || exit
 
 FILENAME="gh_${VERSION}_linux_amd64.deb"
 wget "https://github.com/cli/cli/releases/download/v${VERSION}/${FILENAME}"
 sudo dpkg -i "${FILENAME}"
-/bin/rm "${FILENAME}"
+echo "${FILENAME}"
+# /bin/rm "${FILENAME}"
