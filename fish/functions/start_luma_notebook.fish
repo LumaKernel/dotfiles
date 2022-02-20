7bc988ab4ce804a5da4c369f22a82dec48566e2d
Luma <world@luma.email>
2022-02-21 02:58:03 +0900

    .

--- fish/functions/start_luma_notebook.fish
+++ fish/functions/start_luma_notebook.fish
@@ -3,6 +3,6 @@ function start_luma_notebook
     echo "[error] LUMA_POWER is not 1. Luma notebook needs huge power."
     return 1
   end
-  mkdir -p "$HOME/luma-notebooks"
-  docker run -p 10001:8888 -v "$HOME/luma-notebooks:/home/luma/luma-notebooks" lumakernel/luma-notebooks
+  mkdir -p "$HOME/luma-notebooks" || true
+  docker run -p 10001:8888 -v "$HOME/luma-notebooks:/home/luma/luma-notebooks" lumakernel/luma-notebook
 end
