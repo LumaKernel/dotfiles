972377265e86d339b1adb3d3589ad94937ba995f
Luma <world@luma.email>
2022-06-09 11:30:33 +0900

    .

--- linux/bin/installers/install-rustup.sh
+++ linux/bin/installers/install-rustup.sh
@@ -5,4 +5,4 @@ SCRIPTS_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 # shellcheck source=../../utils/shared.sh
 source "$SCRIPTS_DIR/utils/shared.sh"
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --quiet --no-modify-path
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
