d6fdac9098427ca49142c6b244cae5c90260cd65
Luma <world@luma.email>
2022-06-09 13:29:36 +0900

    .

--- linux/bin/installers/install-speedtest.sh
+++ linux/bin/installers/install-speedtest.sh
@@ -7,7 +7,7 @@ SCRIPTS_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 # shellcheck source=../../utils/shared.sh
 source "$SCRIPTS_DIR/utils/shared.sh"
 
-verify_host install.speedtest.net:443 "4fe715cbcfc7589d9996a274cfa7e70192743628d4cf85d61080d848759f4c32"
+# verify_host install.speedtest.net:443 "4fe715cbcfc7589d9996a274cfa7e70192743628d4cf85d61080d848759f4c32"
 
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest
