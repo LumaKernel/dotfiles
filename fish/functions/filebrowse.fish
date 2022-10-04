0d356c2ae3517bae4a50d5263ae371405d4883e6
Luma <world@luma.email>
2022-10-04 14:34:56 +0900

    default filebrowse func

--- fish/functions/filebrowse.fish
+++ fish/functions/filebrowse.fish
@@ -1,3 +1,8 @@
 function filebrowse
-  filebrowser -p 9100 -a 0.0.0.0 -r $argv
+  echo "username: admin; password: admin"
+  set -l args $argv
+  if test (count $args) = 0
+    set args "."
+  end
+  filebrowser -p 9100 -a 0.0.0.0 -r $args
 end
