0d62792936e7e304888362c55022420a17225ca3
Luma <world@luma.email>
2023-11-05 18:33:49 +0900

    fish function update

--- fish/functions/cargo_watch_error.fish
+++ fish/functions/cargo_watch_error.fish
@@ -1,1 +1,3 @@
-alias cargo_watch_error='RUSTFLAGS=-Awarnings cargo watch -x check'
+function cargo_watch_error --wraps='cargo watch -x check' --description='Run cargo watch check with warnings disabled'
+  RUSTFLAGS=-Awarnings cargo watch -x check
+end
