1b650a2a9e0f63c7c012ecb69026b14ac3cc9544
Luma <world@luma.email>
2024-10-20 19:36:53 +0900

    gc.fishにgit local configのチェックを追加

--- fish/functions/gc.fish
+++ fish/functions/gc.fish
@@ -1,1 +1,7 @@
-alias gc='git commit'
+function gc --wraps='git commit' --description 'alias gc=git commit'
+  if ! git config --local user.email
+    echo "[ERROR] Please set your email address with 'git config --local user.email'"
+    return 1
+  end
+  git commit $argv
+end
