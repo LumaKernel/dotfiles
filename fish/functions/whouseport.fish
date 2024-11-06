36c42bb7ed2ab3d431ae2d0aeced37d7a76d8e17
Luma <world@luma.email>
2024-11-06 18:51:36 +0900

    whouseport.fish mac対応

--- fish/functions/whouseport.fish
+++ fish/functions/whouseport.fish
@@ -1,3 +1,9 @@
-function whouseport
-  sudo netstat -tulpn | grep ":$argv[1] "
+if test "$LUMA_IS_MAC" = "1"
+  function whouseport
+    sudo lsof -i -P | grep LISTEN | grep ":$argv[1] "
+  end
+else
+  function whouseport
+    sudo netstat -tulpn | grep ":$argv[1] "
+  end
 end
