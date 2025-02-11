f17fb8b24ba9c15fb4659602c332dc096de67b52
Luma <world@luma.email>
2025-02-12 02:47:46 +0900

    support emoji jisyo

--- mac/scripts/update_skk_jisyo.sh
+++ mac/scripts/update_skk_jisyo.sh
@@ -4,6 +4,13 @@ MACSKK_DICT_BASE_DIR="$HOME"/Library/Containers/net.mtgto.inputmethod.macSKK/Dat
 mkdir -p "$MACSKK_DICT_BASE_DIR"
 for f in $(find "$HOME"/dotfiles/skk-dict -type f); do
   cp "$f" "$MACSKK_DICT_BASE_DIR"
+  # https://github.com/mtgto/macSKK/issues/234
   # sudo ln -sf "$f" "$MACSKK_DICT_BASE_DIR"/"$(basename "$f")"
 done
 
+f="$HOME/ghq/github.com/LumaKernel/skk-emoji-jisyo/SKK-JISYO.emoji.utf8"
+if [ -f "$f" ]; then
+  cp "$f" "$MACSKK_DICT_BASE_DIR"
+else
+  echo "[WARN] 辞書ファイルが見つかりません: $f"
+fi
