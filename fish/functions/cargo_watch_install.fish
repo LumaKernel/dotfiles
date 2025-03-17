b03af0271a47336876b584946b5c1c74abdf9328
Luma <world@luma.email>
2025-03-18 03:02:49 +0900

    fix fish/functions/cargo_watch_install.fish

--- fish/functions/cargo_watch_install.fish
+++ fish/functions/cargo_watch_install.fish
@@ -1,3 +1,3 @@
 function cargo_watch_install --wraps='cargo watch'
-  cargo_watch -s 'cargo install --path . --locked '$argv
+  cargo_watch -s 'cargo install --path . --locked '(echo $argv | string join ' ')
 end
