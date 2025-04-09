9896cde59e793928710748987be1e8e70bd88c30
Luma <world@luma.email>
2025-04-09 09:15:44 +0900

    fix

--- mac/scripts/install_mas.sh
+++ mac/scripts/install_mas.sh
@@ -6,4 +6,4 @@ mas install 302584613 # Kindle
 mas install 1527619437 # Maccy
 mas install 6446206067 # Klack
 mas install 1444383602 # Goodnotes
-ms install 1475387142 # Tailscale
+mas install 1475387142 # Tailscale
