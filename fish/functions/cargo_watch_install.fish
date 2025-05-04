caa9fa86f15ef9d249900484ed5e89d244b3af3e
Luma <world@luma.email>
2025-05-04 15:27:45 +0900

    migrate cargo_watch_install.fish to watchexec

--- fish/functions/cargo_watch_install.fish
+++ fish/functions/cargo_watch_install.fish
@@ -1,3 +1,3 @@
-function cargo_watch_install --wraps='cargo watch'
-  cargo_watch -s 'cargo install --path . --locked '(echo $argv | string join ' ')
+function cargo_watch_install --wraps='watchexec' --description 'Install cargo plugins with watchexec'
+  watchexec -e rs -r -w src -w Cargo.toml -w Cargo.lock --clear -- cargo install --path . --locked $argv
 end
