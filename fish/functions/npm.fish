80a3e88559fbd09b36e40ade89f46013dbe85aa0
Luma <world@luma.email>
2025-05-30 14:27:28 +0900

    npm/npx; good bye, corepack

--- fish/functions/npm.fish
+++ fish/functions/npm.fish
@@ -1,5 +1,12 @@
-if command -v corepack > /dev/null
-  function npm --wraps=npm
-    time corepack npm $argv
+function npm --wraps=npm
+  set -l with_time 1
+  if contains -- --version $argv
+      or contains -- -v $argv
+    set with_time 0
+  end
+  if test $with_time -eq 1
+    time command npm $argv
+  else
+    command npm $argv
   end
 end
--- fish/functions/npx.fish
+++ fish/functions/npx.fish
@@ -1,5 +1,12 @@
-if command -v corepack > /dev/null
-  function npx --wraps=npx
-    time corepack npx $argv
+function npx --wraps=npx
+  set -l with_time 1
+  if contains -- --version $argv
+      or contains -- -v $argv
+    set with_time 0
+  end
+  if test $with_time -eq 1
+    time command npx $argv
+  else
+    command npx $argv
   end
 end
