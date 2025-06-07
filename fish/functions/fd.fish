0d91d62c4463edacc71d65084f7e342a42cbbc1c
Luma <world@luma.email>
2025-06-08 02:56:36 +0900

    fdの新しいfish completion, fzf.fishの追加

--- fish/functions/fd.fish
+++ fish/functions/fd.fish
@@ -1,4 +1,5 @@
 if ! type fd >/dev/null 2>&1
+  source (fd --gen-completions=fish | psub)
   if command -v fdfind >/dev/null 2>&1
     alias fd=fdfind
   end
--- fish/functions/install_fisher_plugins.fish
+++ fish/functions/install_fisher_plugins.fish
@@ -2,8 +2,8 @@ function install_fisher_plugins
   fisher install LumaKernel/to-fish
   fisher install jorgebucaran/fish-nvm
   fisher install LumaKernel/fish-gulp-complete
-  fisher install LumaKernel/fish-fd-complete
   fisher install otms61/fish-pet
+  fisher install PatrickF1/fzf.fish
 
   fisher list
 end
